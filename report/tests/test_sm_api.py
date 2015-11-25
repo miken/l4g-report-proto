ef88f67baa7ccf0ddaec02be7a7aef027988191d
Mike Nguyen <mike@toch.im>
2015-11-25 13:50:55 -0800

    Complete tests for SM API

--- report/tests/test_sm_api.py
+++ report/tests/test_sm_api.py
@@ -21,3 +21,33 @@ class SMClientTestCase(TestCase):
         survey_list = self.api_client.get_survey_list(self.survey_name)
         expected = [{u'survey_id': u'72140246'}]
         self.assertEqual(survey_list, expected)
+
+    def test_get_survey_id(self):
+        '''
+        The client should return only one single ID
+        for Mike Test - DO NOT USE
+        '''
+        survey_id = self.api_client.get_survey_id(self.survey_name)
+        self.assertEqual(survey_id, u'72140246')
+
+    def test_get_survey_details(self):
+        survey_pages = self.api_client.get_survey_details(self.survey_name)
+        # survey_pages is a list
+        self.assertTrue(isinstance(survey_pages, list))
+        # There is only 1 element/page
+        self.assertEqual(len(survey_pages), 1)
+        # Check each element of pages to make sure they contain the correct keys
+        expected_page_keys = [u'position', u'sub_heading', u'page_id', u'heading', u'questions']
+        for p in survey_pages:
+            self.assertEqual(p.keys(), expected_page_keys)
+
+    def test_get_respondent_list(self):
+        respondent_ids = self.api_client.get_respondent_list(self.survey_name)
+        expected = [u'4352787778', u'4352787305', u'4352786821', u'4352786417', u'4352785923']
+        self.assertEqual(respondent_ids, expected)
+
+    def test_get_responses(self):
+        responses = self.api_client.get_responses(self.survey_name)
+        # Check Respondent IDs
+        respondent_ids = [r['respondent_id'] for r in responses]
+        expected_resp_ids = [u'4352787778', u'4352787305', u'4352786821', u'4352786417', u'4352785923']
