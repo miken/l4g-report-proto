183831ef8084c1b4bcc0438a0ba3b2e4cdc0127d
Mike Nguyen <mike@toch.im>
2015-11-26 16:49:58 -0800

    Wire up survey detail view

--- report/urls.py
+++ report/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.SurveyList.as_view()),
+    url(r'^$', views.SurveyList.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.SurveyDetail.as_view(), name='detail'),
 ]
--- report/views.py
+++ report/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from .models import Survey
 
 
 class SurveyList(ListView):
+    model = Survey
+
+
+class SurveyDetail(DetailView):
     model = Survey
